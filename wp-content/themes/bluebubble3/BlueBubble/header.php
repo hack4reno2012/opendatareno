bfc1448a1d2efe4e49f8bf9962c4f558959cb850
root <root@geekreno.com>
2012-12-01 15:46:24 -0800

    added favicon

--- wp-content/themes/bluebubble3/BlueBubble/header.php
+++ wp-content/themes/bluebubble3/BlueBubble/header.php
@@ -38,8 +38,7 @@
 <?php } // Check for Colorbox Off ?>        
 
 <!-- ** Links ** -->
-
-<link rel="shortcut icon" href="<?php echo get_option('bb_favicon'); ?>" type="image/x-icon"/>
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
