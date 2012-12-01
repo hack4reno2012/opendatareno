a5568e9dc7256101c5d8ab13064d1b1ed77c62b8
root <root@geekreno.com>
2012-12-01 15:18:24 -0800

    removed extraneous themes

--- wp-content/themes/academica/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php 
-global $options;
-foreach ($options as $value) {
-    if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
-}
-?>
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar') ) : ?> <?php endif; ?>
