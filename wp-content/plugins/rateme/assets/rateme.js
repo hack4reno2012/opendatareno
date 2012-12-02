69cb6b5b83cdc32674861abb213c5f707c31c35e
Colin Loretz <colin@livelylabs.com>
2012-12-02 04:58:44 -0800

    added css/js

---
+++ wp-content/plugins/rateme/assets/rateme.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+	$(".rating span").click(function(){
+		$(this).removeClass("selected");
+		$(this).siblings().removeClass("selected");
+		
+		var num = $(this).attr("class");
+		if(num == "one") num = 1;
+		if(num == "two") num = 2;
+		if(num == "three") num = 3;
+		if(num == "four") num = 4;
+		if(num == "five") num = 5;
+		$("input[name=wprm_rating]"").val(num);
+		
+		$(this).addClass("selected");
+		$(this).nextAll("span").addClass("selected");
+	});
+
+});
