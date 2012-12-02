f4ad11963486a20bf43eaaf1a4ddb33aaf0f5e79
Colin Loretz <colin@livelylabs.com>
2012-12-02 10:39:15 -0800

    restyle dataset page

--- wp-content/themes/opendatacity/alldatasets-page.php
+++ wp-content/themes/opendatacity/alldatasets-page.php
@@ -21,12 +21,25 @@ foreach ($options as $value) {
 				$args=array('posts_per_page' => '-1', 'orderby' => 'post_title', 'order' => 'asc' );
 				$allposts = new WP_Query($args); ?>
 			<?php if ( $allposts->have_posts() ) : ?>
-			<ul>
 				<?php while( $allposts->have_posts() ) : $allposts->the_post(); ?>
 					<li><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></li>
+
+					<div class="datasetsingle" id="post-<?php the_ID(); ?>"> 
+			        
+						<div class = "thumb">
+							<a href="<?php the_permalink() ?>" rel="bookmark" title="View detail"><?php the_post_thumbnail('thumbnail'); ?></a> 
+						</div>
+								
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="View detail"><?php the_title(); ?></a><?php if (get_option('bb_no_tweet') == '') { ?><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="<?php echo get_option('bb_twitter_name') ?>">Tweet</a>
+	<?php } ?></h2>
+					
+					<div style = "clear: both"></div>
+ 
+					</div>
+
+
 				<?php endwhile; ?>
 				<?php wp_reset_postdata(); ?>
-			</ul>
 			<?php endif; ?>
 
 	</div>
