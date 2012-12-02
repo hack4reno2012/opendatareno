50fd62d532ecf53767b580ed95cd3b63d2958a0d
Colin Loretz <colin@livelylabs.com>
2012-12-01 23:56:15 -0800

    added colin's rateme plugin

---
+++ wp-content/plugins/rateme/rateme.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package Rate_Me
+ * @version 0.1
+ */
+/*
+Plugin Name: Rate Me
+Plugin URI: https://github.com/colinloretz/rateme
+Description: Post rating plugin built for Open Data Catalog
+Author: Colin Loretz
+Version: 0.1
+Author URI: https://github.com/colinloretz
+*/
+
+function add_rating($comment_id)) {
+
+	// add rating to the comment
+	add_comment_meta($comment_id, 'wprm_rating', $_POST['wprm_rating'], true);
+
+	// calculate_post_rating
+	calculate_post_rating($comment_id);
+}
+
+
+function calculate_post_rating($comment_id) {
+
+	$comment = get_comment($comment_id);
+	$postID = $comment->comment_post_ID;
+
+   	// get all comments for this post
+	$post_comments = get_approved_comments($postID);
+
+	$sum = 0;
+
+	// get the sum for all ratings for all comments
+	foreach($post_comments as $comment) {
+		$comment_rating = get_comment_meta($comment->comment_ID, 'wprm_rating', true);
+		$sum += $comment_rating;
+	}
+
+	// number of comments
+	$count = sizeof($post_comments);
+
+	// calculate average rating for post
+	$post_rating = $sum / $count;
+
+	// update post meta with new post rating
+	update_post_meta($postID, 'wprm_post_rating', $post_rating);
+}
+
+// Comment has been added, save the individual rating and calculate post rating
+add_action('comment_post', 'add_rating');
+
+// Comment has been deleted, recalculate the overall post rating
+add_action('deleted_comment', 'calculate_post_rating');
+
+?>
--- wp-content/themes/opendatacity/comments.php
+++ wp-content/themes/opendatacity/comments.php
@@ -37,7 +37,7 @@
                                                                                                    
 <p><textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea></p>
 
-<p><div style = "float: left; width: 150px;">Rate this data <?php //the_comment_rating(); ?></div>
+<p><div style = "float: left; width: 150px;">Rate this data <input type="text" name="wprm_rating" value="" /></div>
 <input name="submit" type="submit" id="submit" tabindex="5" value="Submit" style = "float: left; margin-top: 10px; padding: 10px;"/><div style = "clear: both"></div>
 <?php comment_id_fields(); ?>
 </p>
